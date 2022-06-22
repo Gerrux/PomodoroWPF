6e7b6814564f18527096cf4bc3e00893b379097c
Gerrux <kalinin.i.v@edu.narfu.ru>
2022-06-23 01:01:25 +0300

    Добавьте файлы проекта.

---
+++ PomodoroWPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace PomodoroWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Draggable window
+        protected void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+        #endregion
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
