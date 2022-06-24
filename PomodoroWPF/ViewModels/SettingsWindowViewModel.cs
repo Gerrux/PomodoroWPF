0c2029fb1eb989f1b8f58e1900123d89beebe993
Gerrux <kalinin.i.v@edu.narfu.ru>
2022-06-25 00:44:37 +0300

    [Update] Settings window

--- PomodoroWPF/ViewModels/SettingsWindowViewModel.cs
+++ PomodoroWPF/ViewModels/SettingsWindowViewModel.cs
@@ -4,6 +4,48 @@ namespace PomodoroWPF.ViewModels
 {
     internal class SettingsWindowViewModel : ObservableObject
     {
+        private string workMinutes = "25";
 
+        public string WorkMinutes
+        {
+            get => workMinutes;
+            set
+            {
+                SetProperty(ref workMinutes, value);
+            }
+        }
+
+        private string workSeconds = "00";
+
+        public string WorkSeconds
+        {
+            get => workSeconds;
+            set
+            {
+                SetProperty(ref workSeconds, value);
+            }
+        }
+
+        private string restMinutes = "05";
+
+        public string RestMinutes
+        {
+            get => restMinutes;
+            set
+            {
+                SetProperty(ref restMinutes, value);
+            }
+        }
+
+        private string restSeconds = "00";
+
+        public string RestSeconds
+        {
+            get => restSeconds;
+            set
+            {
+                SetProperty(ref restSeconds, value);
+            }
+        }
     }
 }
